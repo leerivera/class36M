1608f132498036b7caf2171ce65a4b835f96f4f5
leerivera <leerivera80@gmail.com>
2022-01-10 07:08:09 -0500

    now

--- class36-materials/node-backend-simple-json/server.js
+++ class36-materials/node-backend-simple-json/server.js
@@ -56,12 +56,12 @@ const server = http.createServer((req, res) => {
       res.write(data);
       res.end();
     });
-  }else if (page == '/js/main.js'){
-    fs.readFile('js/main.js', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/javascript'});
-      res.write(data);
-      res.end();
-    });
+  // }else if (page == '/js/main.js'){
+  //   fs.readFile('js/main.js', function(err, data) {
+  //     res.writeHead(200, {'Content-Type': 'text/javascript'});
+  //     res.write(data);
+  //     res.end();
+  //   });
   }else{
     figlet('404!!', function(err, data) {
       if (err) {
