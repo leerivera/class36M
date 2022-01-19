634293bd4ff66abc66a58d692dd56c35fcd66150
leerivera <leerivera80@gmail.com>
2022-01-19 07:09:09 -0500

    more node

--- class36-materials/node-backend-simple-json/server.js
+++ class36-materials/node-backend-simple-json/server.js
@@ -15,13 +15,13 @@ const server = http.createServer((req, res) => {
       res.end();
     });
   }
-  else if (page == '/otherpage') {
-    fs.readFile('otherpage.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
+  // else if (page == '/otherpage') {
+  //   fs.readFile('otherpage.html', function(err, data) {
+  //     res.writeHead(200, {'Content-Type': 'text/html'});
+  //     res.write(data);
+  //     res.end();
+  //   });
+  // }
   else if (page == '/otherotherpage') {
     fs.readFile('otherotherpage.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
