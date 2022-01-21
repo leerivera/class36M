c07009148463fe55099388d1f7b381be8b9008d8
leerivera <leerivera80@gmail.com>
2022-01-21 07:09:09 -0500

    node

--- class36-materials/node-backend-simple-json/server.js
+++ class36-materials/node-backend-simple-json/server.js
@@ -4,17 +4,17 @@ const url = require('url');
 const querystring = require('querystring');
 const figlet = require('figlet')
 
-const server = http.createServer((req, res) => {
-  const page = url.parse(req.url).pathname;
-  const params = querystring.parse(url.parse(req.url).query);
-  console.log(page);
-  if (page == '/') {
-    fs.readFile('index.html', function(err, data) {
-      res.writeHead(200, {'Content-Type': 'text/html'});
-      res.write(data);
-      res.end();
-    });
-  }
+// const server = http.createServer((req, res) => {
+//   const page = url.parse(req.url).pathname;
+//   const params = querystring.parse(url.parse(req.url).query);
+//   console.log(page);
+//   if (page == '/') {
+//     fs.readFile('index.html', function(err, data) {
+//       res.writeHead(200, {'Content-Type': 'text/html'});
+//       res.write(data);
+//       res.end();
+//     });
+//   }
   else if (page == '/otherpage') {
     fs.readFile('otherpage.html', function(err, data) {
       res.writeHead(200, {'Content-Type': 'text/html'});
