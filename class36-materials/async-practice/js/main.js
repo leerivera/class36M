c8b43bd8817222f637fb4bfd3171651e37c6d616
leerivera <leerivera80@gmail.com>
2022-01-28 07:09:09 -0500

    code12

--- class36-materials/async-practice/js/main.js
+++ class36-materials/async-practice/js/main.js
@@ -123,14 +123,14 @@ function houseThree(){
     })
 }
 
-// async function getPaid(){
-//     const houseOneWait = await houseOne()
-//     const houseTwoWait = await houseTwo()
-//     const houseThreeWait = await houseThree()
-//     console.log(houseOneWait)
-//     console.log(houseTwoWait)
-//     console.log(houseThreeWait)
-// }
+async function getPaid(){
+    const houseOneWait = await houseOne()
+    const houseTwoWait = await houseTwo()
+    const houseThreeWait = await houseThree()
+    console.log(houseOneWait)
+    console.log(houseTwoWait)
+    console.log(houseThreeWait)
+}
 
 // getPaid()
 
