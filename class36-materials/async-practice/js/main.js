8d8bbaa9dea668f09ec99c89725f2c6976ea08b1
leerivera <leerivera80@gmail.com>
2022-05-25 15:22:49 -0400

    code13

--- class36-materials/async-practice/js/main.js
+++ class36-materials/async-practice/js/main.js
@@ -134,11 +134,11 @@ async function getPaid(){
 
 getPaid()
 
-//Code 08
-// async function getACuteDogPhoto(){
-//     const res = await fetch('https://dog.ceo/api/breeds/image/random')
-//     const data = await res.json()
-//     console.log(data)
-// }
+Code 08
+async function getACuteDogPhoto(){
+    const res = await fetch('https://dog.ceo/api/breeds/image/random')
+    const data = await res.json()
+    console.log(data)
+}
 // getACuteDogPhoto()
 
